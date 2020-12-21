b3d15bc8676c2e6abb1c9494becc929414a86db3
Edmond <edmond.b@hotmail.co.uk>
2020-12-21 15:13:18 +0000

    all passing tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,19 @@ require 'fizzbuzz'
 # same as require_relative '../lib/fizzbuzz.rb'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiples 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed multiples 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed multiples of 3 & 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "number" when passed anything else' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
